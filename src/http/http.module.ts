778a399898b94966980b22751d562e3c9255c8eb
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-14 19:56:39 -0300

    first commit

---
+++ src/http/http.module.ts
@@ -0,0 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'node:path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.ggl'),
+    }),
+  ],
+})
+export class HttpModule {}
