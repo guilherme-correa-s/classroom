a023482b86597435cb95dd5d712dc1ce9c1e1176
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-16 14:32:05 -0300

    refactor: http module

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -2,15 +2,31 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import path from 'node:path';
+import { join } from 'path';
+import { DatabaseModule } from 'src/database/database.module';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.ggl'),
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    CoursesResolver,
+    CoursesService,
+    EnrollmentsResolver,
+    EnrollmentsService,
+    StudentsResolver,
+    StudentsService,
   ],
 })
 export class HttpModule {}
